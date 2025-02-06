c43f489c20bb26cd20ad875fc375277f68bb3fdd
fitaratraHoAnao <brunorakotomalala113@gmail.com>
2025-02-06 08:27:00 +0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -39,7 +39,7 @@ generation_config = {
 }
 
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash",
+    model_name="gemini-2.0-pro-exp-02-05",
     generation_config=generation_config,
 )
 
