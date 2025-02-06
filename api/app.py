d7e4956c2d90cb46b526328db73cf7081deda757
fitaratraHoAnao <brunorakotomalala113@gmail.com>
2025-02-06 16:21:37 +0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -30,17 +30,18 @@ def upload_to_gemini(path, mime_type=None):
     return file
 
 # Configuration du modèle avec les paramètres de génération
+# Create the model
 generation_config = {
-    "temperature": 1,
-    "top_p": 0.95,
-    "top_k": 64,
-    "max_output_tokens": 8192,
-    "response_mime_type": "text/plain",
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 40,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
 }
 
 model = genai.GenerativeModel(
-    model_name="gemini-2.0-flash",
-    generation_config=generation_config,
+  model_name="gemini-2.0-flash",
+  generation_config=generation_config,
 )
 
 @app.route('/api/gemini', methods=['POST'])
